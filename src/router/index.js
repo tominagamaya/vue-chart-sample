2860f5f15dd1bb7ab0bf051e4ba6a6f77a8a05b4
tominagamaya <maya.tominaga.9@gmail.com>
2018-12-22 20:37:58 +0900

    Homeを追加

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home'
 import VueChartJS from '@/components/VueChartJS'
 import VueChartKick from '@/components/VueChartKick'
 import VueCharts from '@/components/VueCharts'
@@ -11,8 +11,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
     },
     {
       path: '/chartjs',
