bc980e8bb3619c6cb5b9d35f0d00b28e3f675b0b
tominagamaya <maya.tominaga.9@gmail.com>
2018-12-22 20:23:56 +0900

    chartjsを追加

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import VueChartJS from '@/components/VueChartJS'
+import VueChartKick from '@/components/VueChartKick'
+import VueCharts from '@/components/VueCharts'
 
 Vue.use(Router)
 
@@ -10,6 +13,21 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/chartjs',
+      name: 'VueChartJS',
+      component: VueChartJS
+    },
+    {
+      path: '/chartkick',
+      name: 'VueChartKick',
+      component: VueChartKick
+    },
+    {
+      path: '/chart',
+      name: 'VueCharts',
+      component: VueCharts
     }
   ]
 })
